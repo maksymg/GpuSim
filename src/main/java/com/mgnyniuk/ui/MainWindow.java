6ca648e24739cd611874d6d8d53e02fbbbbc0deb
maksymg <mgnyniuk@gmail.com>
2014-03-23 21:40:20 +0200

    Add button for new Experiment

--- src/main/java/com/mgnyniuk/ui/MainWindow.java
+++ src/main/java/com/mgnyniuk/ui/MainWindow.java
@@ -1,7 +1,12 @@
 package com.mgnyniuk.ui;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import javafx.scene.Group;
 
@@ -10,9 +15,22 @@ import javafx.scene.Group;
  */
 public class MainWindow extends Application {
 
+    private static final Image NEW_BTN = new Image(MainWindow.class.getResourceAsStream("/pictures/btn_new.png"));
+
     private void init(Stage primaryStage) {
         Group root = new Group();
-        primaryStage.setScene(new Scene(root));
+        primaryStage.setScene(new Scene(root, 700, 500));
+
+        // New Experiment Button
+        ImageView newBtn = new ImageView(NEW_BTN);
+        Button newExperimentBtn = new Button();
+        newExperimentBtn.setGraphic(newBtn);
+
+        // HBox with spacing 5
+        HBox hBox = new HBox(5);
+        hBox.getChildren().addAll(newExperimentBtn);
+        hBox.setAlignment(Pos.TOP_LEFT);
+        root.getChildren().add(hBox);
     }
 
     @Override
