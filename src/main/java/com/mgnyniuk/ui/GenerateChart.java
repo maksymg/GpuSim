49d3c077ed6c9ec0234c8b7ce3b5d14ee653db22
maksymg <mgnyniuk@gmail.com>
2014-05-11 22:28:42 +0300

    add method for generating charts for Matrix Multiply Exp

--- src/main/java/com/mgnyniuk/ui/GenerateChart.java
+++ src/main/java/com/mgnyniuk/ui/GenerateChart.java
@@ -1,7 +1,49 @@
 package com.mgnyniuk.ui;
 
+import com.gpusim2.config.GridSimOutput;
+import com.mgnyniuk.core.ConfigurationUtil;
+import javafx.scene.chart.AreaChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by maksym on 5/11/14.
  */
 public class GenerateChart {
+
+    public static AreaChart<Number, Number> getResultChartForMatrixMultiplyExperiment(int outputStartIndex,
+                                                                                      int outputEndIndex,
+                                                                                      List<Integer> matrixSizeList) throws FileNotFoundException {
+        NumberAxis xAxis = new NumberAxis();
+        NumberAxis yAxis = new NumberAxis();
+        AreaChart<Number, Number> ac = new AreaChart<Number, Number>(xAxis, yAxis);
+
+        // setup chart
+        ac.setTitle("Simulation Result");
+        xAxis.setLabel("Matrix size");
+        yAxis.setLabel("Time");
+
+        // output list
+        List<GridSimOutput> outputList = ConfigurationUtil.loadOutputs(outputStartIndex, outputEndIndex);
+
+        // matrix size list
+        /*for (int i = 1; i <= 4096 / 16; i++) {
+            matrixSizeList.add(16 * i);
+        } */
+
+        XYChart.Series<Number, Number> series = new XYChart.Series<>();
+        series.setName("Time/Matrix Size");
+
+        for (int i = 0; i < matrixSizeList.size() - 1; i++) {
+            series.getData().add(new XYChart.Data<Number, Number>(matrixSizeList.get(i), outputList.get(i).getTotalSimulationTime()));
+        }
+
+        ac.getData().add(series);
+
+        return ac;
+    }
 }
