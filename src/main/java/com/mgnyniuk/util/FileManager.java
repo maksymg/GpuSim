832f94cfc14a9e198c6a68ab89d04e1c0cbaa6c4
maksymg <mgnyniuk@gmail.com>
2014-05-13 21:49:56 +0300

    Added two methods: deleteConfigFiles deleteOutputFiles

--- src/main/java/com/mgnyniuk/util/FileManager.java
+++ src/main/java/com/mgnyniuk/util/FileManager.java
@@ -1,7 +1,39 @@
 package com.mgnyniuk.util;
 
+import java.io.File;
+
 /**
  * Created by maksym on 5/13/14.
  */
 public class FileManager {
+
+    public static void deleteConfigFiles(int quantity) {
+
+        for (int i = 0; i < quantity; i++) {
+            File configFile = new File("config" + i + ".xml");
+
+            if (configFile.delete()) {
+                System.out.println(configFile.getName() + " is deleted!");
+            } else {
+                System.out.println("Delete operation is failed.");
+            }
+
+        }
+
+    }
+
+    public static void deleteOutputFiles(int quantity) {
+
+        for (int i = 0; i < quantity; i++) {
+            File configFile = new File("output" + i + ".xml");
+
+            if (configFile.delete()) {
+                System.out.println(configFile.getName() + " is deleted!");
+            } else {
+                System.out.println("Delete operation is failed.");
+            }
+
+        }
+
+    }
 }
