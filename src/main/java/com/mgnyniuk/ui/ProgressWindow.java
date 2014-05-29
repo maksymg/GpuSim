4a121598eabb027910093a6fa4cbce2a0e379af7
maksymg <mgnyniuk@gmail.com>
2014-05-29 23:46:26 +0300

    ProgressBar Window

--- src/main/java/com/mgnyniuk/ui/ProgressWindow.java
+++ src/main/java/com/mgnyniuk/ui/ProgressWindow.java
@@ -1,7 +1,35 @@
 package com.mgnyniuk.ui;
 
+import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.layout.GridPane;
+
 /**
  * Created by maksym on 5/29/14.
  */
 public class ProgressWindow {
+
+    public static ProgressBar pb;
+
+    public static Scene getProgressWindowScene() {
+
+        Group progressBarGroup = new Group();
+        GridPane gridPane = new GridPane();
+
+        Button cancelButton = new Button();
+
+        pb = new ProgressBar();
+        pb.setProgress(20);
+
+        gridPane.setPadding(new Insets(10, 10, 10, 10));
+        gridPane.add(pb, 1,1);
+        gridPane.setColumnSpan(pb, 3);
+
+        progressBarGroup.getChildren().add(gridPane);
+
+        return new Scene(progressBarGroup, 400, 200);
+    }
 }
