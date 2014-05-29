755a3fff691bb1be961350cfa66a1f1dfb010ee2
maksymg <mgnyniuk@gmail.com>
2014-05-29 17:36:13 +0300

    Added reading for output from GpuSimV2.jar

--- src/main/java/com/mgnyniuk/core/com/mgnyniuk/core/parallel/WorkerThread.java
+++ src/main/java/com/mgnyniuk/core/com/mgnyniuk/core/parallel/WorkerThread.java
@@ -1,5 +1,10 @@
 package com.mgnyniuk.core.com.mgnyniuk.core.parallel;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+
 /**
  * Created by maksym on 3/24/14.
  */
@@ -20,7 +25,16 @@ public class WorkerThread extends NotifyingThread {
         try {
             Process process = Runtime.getRuntime().exec("java -jar " + runningJar + " " + configFile + " " + outputFile);
             System.out.println("Process with " + configFile + " is started: " + process + "==== Start time: " + System.currentTimeMillis());
+
+            BufferedReader gpuSimV2Output;  // reader for output of process
+            String line;
+
+            gpuSimV2Output = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            while ((line = gpuSimV2Output.readLine()) != null)
+                System.out.println(line);
             process.waitFor();
+
             System.out.println("Exit value of process with " + configFile + ": " + process.exitValue() + "==== Exit time: " + System.currentTimeMillis());
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
