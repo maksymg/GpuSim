903264a4c8fb48ef29f2a661a7de0a34c8490476
maksymg <mgnyniuk@gmail.com>
2014-06-05 23:45:09 +0300

    add class for calibration

---
+++ src/main/java/com/mgnyniuk/experiment/NBodyExperimentCalibration.java
@@ -0,0 +1,48 @@
+package com.mgnyniuk.experiment;
+
+import javafx.scene.control.Label;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Created by maksym on 6/5/14.
+ */
+public class NBodyExperimentCalibration implements Serializable {
+
+    private List<Integer> nList;
+    private List<Integer> tpbList;
+    private List<Double> simulationTimeList;
+
+    public NBodyExperimentCalibration() { }
+
+    public NBodyExperimentCalibration(List<Integer> nList, List<Integer> tpbList, List<Double> simulationTimeList) {
+        this.nList = nList;
+        this.tpbList = tpbList;
+        this.simulationTimeList = simulationTimeList;
+    }
+
+    public List<Integer> getnList() {
+        return nList;
+    }
+
+    public void setnList(List<Integer> nList) {
+        this.nList = nList;
+    }
+
+    public List<Integer> getTpbList() {
+        return tpbList;
+    }
+
+    public void setTpbList(List<Integer> tpbList) {
+        this.tpbList = tpbList;
+    }
+
+    public List<Double> getSimulationTimeList() {
+        return simulationTimeList;
+    }
+
+    public void setSimulationTimeList(List<Double> simulationTimeList) {
+        this.simulationTimeList = simulationTimeList;
+    }
+}
